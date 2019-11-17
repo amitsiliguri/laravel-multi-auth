689b92abbd6787e01b0d475ed81c4b4f503badd6
amitsiliguri <amit_biswas2014@yahoo.in>
2019-11-17 16:42:45 +0530

    fix admin navbar

--- resources/views/admin/components/sidebar.blade.php
+++ resources/views/admin/components/sidebar.blade.php
@@ -3,27 +3,31 @@
     <li class="nav-item">
       <a class="nav-link active" href="#"><span data-feather="home"></span>Dashboard</a>
     </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#"><span data-feather="bar-chart-2"></span>Marketing</a>
-    </li>
   	<li class="nav-item">
       <a class="nav-link" data-toggle="collapse" href="#collapseOne"><span data-feather="book"></span>Catalog</a>
       <div id="collapseOne" class="collapse" data-parent="#accordion">
-          <a class="inner-link" href="#"><span data-feather="folder"></span>Category</a><br>
+          <a class="inner-link" href="#"><span data-feather="folder"></span>Category</a>
           <a class="inner-link" href="#"><span data-feather="shopping-cart"></span>Products</a>
       </div>
     </li>
     <li class="nav-item">
-      <a class="nav-link" data-toggle="collapse" href="#collapseTwo"><span data-feather="file"></span>Order</a>
+      <a class="nav-link" data-toggle="collapse" href="#collapseTwo"><span data-feather="file"></span>Sales</a>
       <div id="collapseTwo" class="collapse" data-parent="#accordion">
-          consectetur adipisicing elit
+        <a class="inner-link" href="#"><span data-feather="folder"></span>Pending Orders</a>
+        <a class="inner-link" href="#"><span data-feather="file"></span>Invoice</a>
+        <a class="inner-link" href="#"><span data-feather="align-left"></span>Shipped</a>
+        <a class="inner-link" href="#"><span data-feather="shopping-cart"></span>Complete Orders</a>
       </div>
     </li>
     <li class="nav-item">
       <a class="nav-link" data-toggle="collapse" href="#collapseThree"><span data-feather="users"></span>Customer</a>
       <div id="collapseThree" class="collapse" data-parent="#accordion">
-          sed do eiusmod tempor incididunt ut
+        <a class="inner-link" href="#"><span data-feather="folder"></span>Category</a>
+        <a class="inner-link" href="#"><span data-feather="shopping-cart"></span>Products</a>
       </div>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#"><span data-feather="bar-chart-2"></span>Marketing</a>
     </li>
   </ul>
 </div>
