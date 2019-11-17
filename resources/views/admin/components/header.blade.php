4f52e12882387d6285616790e5bd1198b3f46a61
amitsiliguri <amit_biswas2014@yahoo.in>
2019-11-17 14:34:39 +0530

    Admin Layouts

---
+++ resources/views/admin/components/header.blade.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow navbar-expand-lg">
+  <a class="navbar-brand col-6 col-sm-3 col-md-2 mr-0" href="#">{{ __('Laracom') }}</a>
+  @auth
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#adminNav" aria-controls="adminNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="adminNav">
+    <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+    <ul class="navbar-nav px-3">
+      <li class="nav-item text-nowrap">
+        <a class="nav-link" href="#" onclick="event.preventDefault();document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+        <form id="logout-form" action="{{ route('admin.logout') }}" method="POST" style="display: none;">
+            @csrf
+        </form>
+      </li>
+    </ul>
+  </div>
+  @endauth
+</nav>
